22897dea2bb6dd942d785d6cfc2ac7d3cde8fa5d
jskim <jskim@naver.com>
2020-06-02 21:04:07 +0900

    react-ace-editor 로 했다

--- src/CodeMirror.js
+++ src/CodeMirror.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import styles from './css/CodeMirror.css';
 
-import ReactMarkdown from 'react-markdown';
-import CodeBlock from './CodeBlock';
 
 
-const input = '<h1> Hello World</h1>\n\n# [This](http://www.naver.com) is a paragraph';
 
 export default class CodeMirror extends React.Component {
    render(){
       return (
          <div className={styles['CodeMirror']}>
-             <ReactMarkdown source={input}
-             renderers = {{code : CodeBlock}}/>
+<p>aaaaaaa</p>
          </div>
             
       );
--- src/CodeWindow.js
+++ src/CodeWindow.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import styles from './css/CodeWindow.css';
 import Package from './Package';
-import CodeMirror from './CodeMirror';
+// import CodeMirror from './CodeMirror';
+
+// import "../codemirror/lib/codemirror.js";
+// import "../codemirror/lib/codemirror.css";
+// import "../codemirror/theme/material.css"
+// import "../codemirror/mode/xml/xml.js";
+
+// import {UnControlled as CodeMirror} from 'react-codemirror2'
+
+import ReactAce from 'react-ace-editor';
 
 export default class CodeWindow extends React.Component {
+    constructor(){
+        super(...arguments);
+        this.onChange = this.onChange.bind(this);
+    }    
+    onChange(newValue, e) {
+       console.log(newValue, e);
+    
+       const editor = this.ace.editor; // The editor object is from Ace's API
+       console.log(editor.getValue()); // Outputs the value of the editor
+    }
+
    render(){
       return (
          <div className={styles['code-window']}>
@@ -22,9 +42,14 @@ export default class CodeWindow extends React.Component {
                 </div>
                 <div className={styles['code']}>
                     <p>코드 위치      </p>
-                    <CodeMirror /> 
-
-
+                    <ReactAce
+                        mode="javascript"
+                        theme="eclipse"
+                        setReadOnly= {false}
+                        onChange={this.onChange}
+                        style={{ height: '400px' }}
+                        ref={instance => { this.ace = instance; }} // Let's put things into scope
+                    />
                 </div>
                 <div className={styles['result']}>
                     <p>코드 결과창</p>
