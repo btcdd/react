f6eaabc41096f98492efcd9b57b3eb9cb3273cb5
taesung <ktsung94@naver.com>
2020-06-02 19:55:17 +0900

    modify css

--- src/Problem.js
+++ src/Problem.js
@@ -3,15 +3,28 @@ import styles from './css/Problem.css';
 
 
 export default class Problem extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+           showInfo: false
+        }
+     }
+  
+     handToggle(event) {
+        this.setState({
+           showInfo: !this.state.showInfo
+        })
+     }
+
     render(){
        return (
             <div className={styles['problem']}>
                <li>
-                    <div className={styles['sub-problem-title']}>
+                    <div className={styles['sub-problem-title']} onClick={this.handToggle.bind(this)}>
                         <p className={styles['problem-index']}>문제 {this.props.index}</p>
-                        <p>{this.props.subtitle}</p>
+                        <p className={styles['subtitle']}>{this.props.subtitle}</p>
                     </div>
-                    <div className={styles['open']}>
+                    <div className={this.state.showInfo ? styles['open'] : styles['close']}>
                         <div className={styles['contents']}>
                             <p className={styles['problem-contents-title']}>문제 내용</p>
                             <p className={styles['problem-contents']}>{this.props.contents}</p>
