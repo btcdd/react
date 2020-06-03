e3e2985d7b3bf61b1601cad917d844ae72c6d047
jskim <jskim@naver.com>
2020-06-03 10:29:29 +0900

    Ace Editor

--- src/CodeWindow.js
+++ src/CodeWindow.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import styles from './css/CodeWindow.css';
 import Package from './Package';
-// import CodeMirror from './CodeMirror';
 
-// import "../codemirror/lib/codemirror.js";
-// import "../codemirror/lib/codemirror.css";
-// import "../codemirror/theme/material.css"
-// import "../codemirror/mode/xml/xml.js";
+import AceEditor from "react-ace";
 
-// import {UnControlled as CodeMirror} from 'react-codemirror2'
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-c_cpp";
 
-import ReactAce from 'react-ace-editor';
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/webpack-resolver"
 
 export default class CodeWindow extends React.Component {
     constructor(){
         super(...arguments);
-        this.onChange = this.onChange.bind(this);
+       
+        
     }    
-    onChange(newValue, e) {
-       console.log(newValue, e);
-    
-       const editor = this.ace.editor; // The editor object is from Ace's API
-       console.log(editor.getValue()); // Outputs the value of the editor
-    }
+
 
    render(){
       return (
@@ -42,14 +36,25 @@ export default class CodeWindow extends React.Component {
                 </div>
                 <div className={styles['code']}>
                     <p>코드 위치      </p>
-                    <ReactAce
-                        mode="javascript"
-                        theme="eclipse"
-                        setReadOnly= {false}
-                        onChange={this.onChange}
-                        style={{ height: '400px' }}
-                        ref={instance => { this.ace = instance; }} // Let's put things into scope
-                    />
+
+                    <AceEditor
+                     placeholder="Placeholder Text"
+                    mode="python"
+                    theme="monokai"
+                    onLoad={this.onLoad}
+                    onChange={this.onChange}
+                    fontSize={24}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={``}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: true,
+                        showLineNumbers: true,
+                        tabSize: 2,
+                        }}/>                    
                 </div>
                 <div className={styles['result']}>
                     <p>코드 결과창</p>
