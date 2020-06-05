d9f103f6edba6606f0767fc70912a429b76f4ca3
jskim <jskim@naver.com>
2020-06-05 17:32:16 +0900

    code tree 코드클릭시 디비 내용 나오는도록 수정

--- src/codetree/FileList.js
+++ src/codetree/FileList.js
@@ -3,12 +3,18 @@ import React from 'react';
 import styles from '../codetree_css/FileList.css';
 import file from '../img/file.png';
 export default class FileList extends React.Component {
+    
+    mouseClickEvent(){
+        
+        this.props['onNotifySaveCodeChange'](this.props.code);
+    }
+
     render(){
-        console.log("FileList Code >>>>", this.props.code);
+        // console.log("FileList Code >>>>", this.props.code);
        return (
         <div className={styles['problem-file']}>
             <ul>
-               <li><img src={file}/>{this.props.fileName}</li>
+               <li onClick={this.mouseClickEvent.bind(this)} ><img src={file}/>{this.props.fileName}</li>
             </ul>
         </div>
              
